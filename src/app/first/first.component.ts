9c38d4a39108cf9fb050fc562774c4dd38063375
Rajan Subedi <rajan.subedi32@gmail.com>
2019-03-14 18:06:48 +0530

    first-commit

---
+++ src/app/first/first.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first',
+  templateUrl: './first.component.html',
+  styleUrls: ['./first.component.css']
+})
+export class FirstComponent implements OnInit {
+  data:String="Hello world";
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
